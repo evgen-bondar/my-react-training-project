0dcf5dfc28635751f3016a33f2269a197b26e9c2
evgen <bondar4678@gmail.com>
2022-12-15 17:55:06 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return <header className='header'>
+    <img src='https://cdn.dribbble.com/userupload/3158902/file/original-7c71bfa677e61dea61bc2acd59158d32.jpg?resize=400x0'></img>
+  </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Profile.css';
+const Profile = () => {
+    return <div className='profile'>
+    <div>
+    {/* <img src={require('./img/profile.jpg').default} alt='mops'/> */}
+    <img src="https://onbog.com/wp-content/uploads/2019/08/mountain-summit-landscape-horizon-sky-stars-b470-1200x480.jpg"alt=""></img>
+    </div>
+    <div>
+      ava + description
+      {/* <img src='https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg'></img> */}
+    </div>
+    <div>
+      my posts
+      <div>new post</div>
+    </div>
+    <div>
+      <div>post 1</div>
+      <div>post 2</div>
+    </div>
+  </div>
+}
+
+export default Profile
